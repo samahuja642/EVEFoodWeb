7446e549b0550e5136ddd5757fa01152bbf79b14
samahuja642 <samahuja642@gmail.com>
2022-07-10 03:09:31 +0530

    First Look of Website using only HTML,CSS,JS

---
+++ script.js
@@ -0,0 +1,17 @@
+let total = 0;
+let content_basket = ""
+
+function update_basket(){
+    document.getElementById("total").innerHTML = total;
+}
+
+update_basket();
+
+function addItem(id){
+    let mealItem = document.getElementById(id);
+    let mealName = mealItem.parentNode.childNodes[1].innerHTML;
+    let mealPrice = parseFloat(mealItem.parentNode.childNodes[3].innerHTML.replace("Rs ",""));
+    total = total + mealPrice;
+    content_basket = content_basket + mealName;
+    update_basket();
+}
