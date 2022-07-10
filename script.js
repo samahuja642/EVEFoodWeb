33c226bb19ed4667a12ee6d22b15504846c1a5ce
samahuja642 <samahuja642@gmail.com>
2022-07-11 03:50:20 +0530

    Responsive kinda and added buttons

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function update_basket(){
     document.getElementById("total").innerHTML = total;
     document.getElementById("billing").innerHTML = "<tr><th>Dish</th><th>Quantity</th><th>Price</th></tr>"
     for (var key in content_basket){
+	document.getElementById(`${key}Number`).innerHTML = content_basket[key][0];
 	if(content_basket[key][0]!=0){
 	    document.getElementById("billing").innerHTML += `<tr><td>${key}</td><td>${content_basket[key][0]}</td><td>${content_basket[key][1]}</td></tr>`
 	}
@@ -16,6 +17,18 @@ function update_basket(){
 }
 
 update_basket();
+
+function removeItem(id){
+    let mealItem = document.getElementById(id);
+    let mealName = mealItem.parentNode.childNodes[1].innerHTML;
+    let mealPrice = parseFloat(mealItem.parentNode.childNodes[5].innerHTML.replace("Rs ",""));
+    if(content_basket[mealName][0]>0){
+	total = total - mealPrice;
+	content_basket[mealName][0]--;
+	update_basket();
+    }
+}
+    
 
 function addItem(id){
     let mealItem = document.getElementById(id);
