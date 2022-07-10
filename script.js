c7a76bbe233ebb3a9c717eb8c7f9c48b5a5e4214
samahuja642 <samahuja642@gmail.com>
2022-07-10 20:27:34 +0530

    Full fledge Idea implemented acc to me.

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let total = 0;
-let content_basket = ""
+let content_basket = {
+    "Burger":[0,50],
+    "Pao Bhaji":[0,120],
+    "Omlet":[0,60]
+};
 
 function update_basket(){
     document.getElementById("total").innerHTML = total;
+    document.getElementById("billing").innerHTML = "<tr><th>Dish</th><th>Quantity</th><th>Price</th></tr>"
+    for (var key in content_basket){
+	if(content_basket[key][0]!=0){
+	    document.getElementById("billing").innerHTML += `<tr><td>${key}</td><td>${content_basket[key][0]}</td><td>${content_basket[key][1]}</td></tr>`
+	}
+    }
 }
 
 update_basket();
@@ -12,12 +22,16 @@ function addItem(id){
     let mealName = mealItem.parentNode.childNodes[1].innerHTML;
     let mealPrice = parseFloat(mealItem.parentNode.childNodes[5].innerHTML.replace("Rs ",""));
     total = total + mealPrice;
-    content_basket = content_basket + mealName;
+    content_basket[mealName][0]++;
     update_basket();
 }
 
 function checkOut(){
     total = 0;
-    content_basket = "";
+    content_basket = {
+	"Burger":[0,50],
+	"Pao Bhaji":[0,120],
+	"Omlet":[0,60]
+    };
     update_basket();
 }
